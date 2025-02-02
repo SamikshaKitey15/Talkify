f009a4acae31c6d81dce54c11c90b5be87f72e44
Samiksha Kitey <samikshakitey15@gmail.com>
2025-02-02 11:47:16 +0530

    change env

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,7 @@
 let IS_PROD=true;
 const server= IS_PROD ?
-  "https://talkify-backend-90s0.onrender.com" :
+  "https://talkify-backend-m34q.onrender.com" :
   "http://localhost:8000"
 
   export default server;
+  
